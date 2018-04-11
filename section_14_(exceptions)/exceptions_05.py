f4eb3181975ee56cbff9615febdd3989481cd1ef
Matt Pazoles <pazoles@gmail.com>
2018-04-11 15:14:38 -0400

    Python3 Syntax

--- section_14_(exceptions)/exceptions_05.py
+++ section_14_(exceptions)/exceptions_05.py
@@ -8,13 +8,13 @@ try:
     user_input = int(user_input)
 except ValueError:
     try:
-        print "User input was either a float or a string."
+        print("User input was either a float or a string.")
         user_input = int(float(user_input))
-        print "Turns out it was a float! {0}".format(user_input)
+        print("Turns out it was a float! {0}".format(user_input))
     except ValueError:
-        print "Guess {0} was a string and not a number at all.".format(user_input)
+        print("Guess {0} was a string and not a number at all.".format(user_input))
 
-print "Now the code block above works pretty much the same as the following: "
+print("Now the code block above works pretty much the same as the following: ")
 
 user_input = raw_input(" Example #5: enter a number: ")
 
